8488f4e426aafe6c9c69ef608d49d7a5b2dc0246
Dmitri Carpov <dmitri@atlavis.tech>
2025-03-12 21:44:52 -0400

    blink cursor in normal mode

--- .config/nvim/lua/custom/vim-options.lua
+++ .config/nvim/lua/custom/vim-options.lua
@@ -17,7 +17,7 @@ map('n', '<C-A-k>', '<Cmd>BufferMoveNext<CR>', map_opts)
 map('n', '<C-q>', '<Cmd>BufferClose<CR>', map_opts)
 map('n', '<C-x>', '<Cmd>BufferCloseAllButCurrent<CR>', map_opts)
 
-vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
+vim.opt.guicursor = 'n-v-c-sm:block-Cursor-blinkwait175-blinkoff150-blinkon175,i-ci-ve:ver25,r-cr-o:hor20'
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
