30a571c3114c54087b148787495c9509e9af30ca
Dmitri Carpov <dmitri@atlavis.tech>
2025-03-16 12:27:30 -0400

    add curl installation and remove relative line numbers

--- .config/nvim/lua/custom/vim-options.lua
+++ .config/nvim/lua/custom/vim-options.lua
@@ -22,6 +22,3 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-
-vim.opt.relativenumber = true
-vim.o.statuscolumn = '%3r %l %s' -- Add both absolute and relative line numbers
