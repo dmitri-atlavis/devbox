eb6ce01465563b52fe06407d4ef518a4aa94ea95
Dmitri Carpov <dmitri@atlavis.tech>
2025-03-28 09:19:26 -0400

    update vim options

--- .config/nvim/lua/custom/vim-options.lua
+++ .config/nvim/lua/custom/vim-options.lua
@@ -1,5 +1,4 @@
 local map = vim.api.nvim_set_keymap
-local map_opts = { noremap = true, silent = true }
 
 vim.o.guicursor = 'n-v-c-sm:block-Cursor-blinkwait175-blinkoff150-blinkon175,i-ci-ve:ver25,r-cr-o:hor20'
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
@@ -8,8 +7,9 @@ vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- Git blame
-map('n', '<C-g>', '<CMD>GitBlameToggle<CR>', map_opts)
+map('n', '<C-g>', '<CMD>GitBlameToggle<CR>', { desc = 'Git blame' })
+map('n', '<C-x>', '<CMD>bd<CR>', { desc = 'Delete buffer' })
 map('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open directory' })
-map('n', '<Leader>sb', '<CMD>Telescope file_browser<CR>', map_opts)
-map('n', '<Leader>s,', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', map_opts)
-map('n', '<Leader><Leader>', ':lua require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true })<CR>', map_opts)
+map('n', '<Leader>b', '<CMD>Telescope file_browser<CR>', { desc = 'File [B]rowser' })
+map('n', '<Leader>sF', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', { desc = '[S]earch Hidden [F]iles' })
+map('n', '<Leader><Leader>', ':lua require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true })<CR>', { desc = 'Open Buffers' })
