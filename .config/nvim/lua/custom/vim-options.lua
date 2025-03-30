6074d35b1234f321d9c3e65ae280058696574189
Dmitri Carpov <dmitri@atlavis.tech>
2025-03-29 20:32:12 -0400

    go back to neotree and fix telescope buffer mode

--- .config/nvim/lua/custom/vim-options.lua
+++ .config/nvim/lua/custom/vim-options.lua
@@ -6,17 +6,20 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
--- Git blame
+-- Mapping from plugins
 map('n', '<C-g>', '<CMD>GitBlameToggle<CR>', { desc = 'Git blame' })
+
+-- Buffers
 map('n', '<C-x>', '<CMD>bd<CR>', { desc = 'Delete buffer' })
 map('n', '<C-x><C-a>', '<CMD>%bd<CR>', { desc = 'Delete all buffers' })
 map('n', '<C-q>', '<CMD>%bd|e#<CR>', { desc = 'Delete all buffers except this one' })
-map('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open directory' })
-map('n', '<Leader>b', '<CMD>Telescope file_browser<CR>', { desc = 'File [B]rowser' })
-map('n', '<Leader>sF', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', { desc = '[S]earch Hidden [F]iles' })
 map(
   'n',
   '<Leader><Leader>',
-  ':lua require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true, initial_mode="n" })<CR>',
+  ':lua require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true, initial_mode="normal" })<CR>',
   { desc = 'Open Buffers' }
 )
+-- File System
+map('n', '/', '<CMD>Neotree<CR>', { desc = 'Neotree' })
+map('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open directory' })
+map('n', '<Leader>sF', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', { desc = '[S]earch Hidden [F]iles' })
